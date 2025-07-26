0d7dd4a4c66f897ae1baf3f33d8843b04120fa01
thalesaugustodias <thalesaugustodias98@gmail.com>
2025-07-26 15:26:05 -0300

    Ajustes nas configurações de CORS e logging
    Alterado a configuração de CORS para permitir apenas a origem específica `https://cadastro-de-pessoas-vina.onrender.com` nos arquivos `appsettings.Production.json` e `appsettings.json`. O valor de `ExpiracionHoras` foi modificado de `24` para `1` no ambiente de produção. O nível de log padrão foi ajustado de `Warning` para `Information` no arquivo de desenvolvimento.

--- CadastroDePessoas.API/Configuracoes/CorsConfiguracao.cs
+++ CadastroDePessoas.API/Configuracoes/CorsConfiguracao.cs
@@ -1,11 +1,13 @@
-﻿namespace CadastroDePessoas.API.Configuracoes
+﻿using static System.Net.WebRequestMethods;
+
+namespace CadastroDePessoas.API.Configuracoes
 {
     public static class CorsConfiguracao
     {
         public static IServiceCollection AdicionarCorsConfiguracao(this IServiceCollection services, IConfiguration configuration)
         {
             var corsOrigins = configuration.GetSection("Cors:Origins").Get<string[]>() 
-                ?? ["http://localhost:3000", "https://localhost:3000", "http://localhost:3001", "https://localhost:3001"];
+                ?? ["http://localhost:3000", "https://localhost:3000", "http://localhost:3001", "https://localhost:3001", "https://cadastro-de-pessoas-vina.onrender.com"];
 
             services.AddCors(options =>
             {
