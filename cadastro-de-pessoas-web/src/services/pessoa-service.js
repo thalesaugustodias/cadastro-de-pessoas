f7f07c3f350c1ee8c73251fb3ccd5f198f9395e6
thalesaugustodias <thalesaugustodias98@gmail.com>
2025-07-26 21:45:51 -0300

    Atualiza rotas da API para incluir versão v1
    As alterações no arquivo `pessoa-service.js` modificam todas as chamadas para a API, substituindo o caminho `/pessoas` pelo novo caminho `/api/v1/pessoas`. Isso abrange as funções `listar`, `obterPorId`, `criar`, `atualizar` e `remover`, garantindo a conformidade com a nova estrutura de versão da API.

--- cadastro-de-pessoas-web/src/services/pessoa-service.js
+++ cadastro-de-pessoas-web/src/services/pessoa-service.js
@@ -2,27 +2,27 @@ import api from './api';
 
 export const pessoaService = {
     listar: async () => {
-        const response = await api.get('/pessoas');
+        const response = await api.get('/api/v1/pessoas');
         return response.data;
     },
 
     obterPorId: async (id) => {
-        const response = await api.get(`/pessoas/${id}`);
+        const response = await api.get(`/api/v1/pessoas/${id}`);
         return response.data;
     },
 
     criar: async (pessoa) => {
-        const response = await api.post('/pessoas', pessoa);
+        const response = await api.post('/api/v1/pessoas', pessoa);
         return response.data;
     },
 
     atualizar: async (pessoa) => {
-        const response = await api.put('/pessoas', pessoa);
+        const response = await api.put('/api/v1/pessoas', pessoa);
         return response.data;
     },
 
     remover: async (id) => {
-        const response = await api.delete(`/pessoas/${id}`);
+        const response = await api.delete(`/api/v1/pessoas/${id}`);
         return response.data;
     },
 };
